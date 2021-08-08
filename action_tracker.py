cddceb12122f96ee89d7a6cb2045dc0675493053
Taylor <taylorkaake@gmail.com>
2021-08-07 20:35:58 -0400

    Functionality Complete, need to write test cases and cement what exceptions/fringe cases need to be caught. Trying to make as few assumptions as possible.

--- action_tracker.py
+++ action_tracker.py
@@ -1,9 +1,12 @@
 import threading
-from time import perf_counter_ns
+
 
 class ActionTrackerPost:
-    # this implementation is meant to show post processing of action data, average will be calculated on call of
-    # get_stats().  Not ideal as full computational load is during one call.
+    # This implementation is meant to show post processing of action data. Each action "event" is stored and so
+    # this class is extensible to provide other measurements as desired. However, averaging is calculated on call of
+    # get_stats() only.  No computation is done on add_action call, therefore get_stats will take longer to process
+    # dependent upon how many actions have been added.
+
     def __init__(self):
         self.lock = threading.Lock()
         self.actions = {}
@@ -13,27 +16,24 @@ class ActionTrackerPost:
         # time_start = perf_counter_ns()
 
         # Due to concurrency concerns, set a lock to prevent race conditions
-        # print("Awaiting lock")
+
         self.lock.acquire()
 
         try:
-            # print("Lock Acquired")
+
             # Typical input format is already in the form of a python dict, no need to use json module to parse
             action = json_action["action"]
             time = json_action["time"]
-
 
             if action in self.actions.keys():
                 # if this action type exists in the "actions" dict, append this new time to the existing list of times
                 # associated with that unique action
 
-                act_list = self.actions[action]
-                act_list.append(time)
-                self.actions[action] = act_list
+                time_list = self.actions[action]
+                time_list.append(time)
+                self.actions[action] = time_list
             else:
                 self.actions[action] = [time]
-
-            # print(self.actions)
 
         # TODO handle specific exceptions rather than a catch-all
         except:
@@ -42,21 +42,78 @@ class ActionTrackerPost:
         finally:
             self.lock.release()
 
-            # print("Lock released")
-            # time_stop = perf_counter_ns()
-            # print(time_stop - time_start)
+    def get_stats(self):
+        # compute the average time of each unique "action"
 
-    # def get_stats(self):
-    #     # compute the average time of each unique "action"
+        avg_dict = {}
+        self.lock.acquire()
 
+        try:
+            if self.actions:
+                for _ in self.actions.keys():
+                    avg_dict[_] = (sum(self.actions[_])/len(self.actions[_]))
 
-
-
-
-
-
-
-
-
+        finally:
+            self.lock.release()
+        return avg_dict
 
 
+class ActionTrackerRunning:
+    # This implementation does not store each action "event", but rather keeps a running sum of the time values
+    # associated with each unique action, along with how many times that unique action has been added.
+    # The downside is that this is solely useful for calculating the average, and so other metrics cannot be added.
+    # While the :add_action" is marginally slower than the post-processing implementation, the "get_stats" function here
+    # performs significantly better, as only one calculation is being done per unique action.
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.actions = {}
+
+    def add_action(self, json_action):
+        # Attempts to parse the input string, appends ("action", time) to actions] if successful
+        # time_start = perf_counter_ns()
+
+        # Due to concurrency concerns, set a lock to prevent race conditions
+
+        self.lock.acquire()
+
+        try:
+
+            # Typical input format is already in the form of a python dict, no need to use json module to parse
+            action = json_action["action"]
+            time = json_action["time"]
+
+            if action in self.actions.keys():
+                # if this action type exists in the "actions" dict, append this new time to the existing list of times
+                # associated with that unique action
+
+                time_sum = self.actions[action][0]
+                time_sum += time
+                action_count = self.actions[action][1]
+                action_count += 1
+                self.actions[action] = [time_sum, action_count]
+            else:
+                self.actions[action] = [time, 1]
+
+        # TODO handle specific exceptions rather than a catch-all
+        except:
+            raise ValueError("JSON improperly formatted or missing keys, please check input format")
+
+        finally:
+            self.lock.release()
+
+    def get_stats(self):
+        # compute the average time of each unique "action"
+
+        avg_dict = {}
+        self.lock.acquire()
+
+        try:
+            if self.actions:
+                for x in self.actions.keys():
+
+                    avg_dict[x] = self.actions[x][0]/self.actions[x][1]
+
+        finally:
+            self.lock.release()
+        return avg_dict
--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 import random
-from action_tracker import ActionTrackerPost
+from time import perf_counter_ns
+from action_tracker import ActionTrackerPost, ActionTrackerRunning
 
 
 if __name__ == '__main__':
     Action_Tracker = ActionTrackerPost()
-    Action_Tracker.add_action({"action":"jump", "time":100})
-    for i in range(1000000):
+    Action_Tracker2 = ActionTrackerRunning()
+    for i in range(100000):
+        randnumber = random.randint(0, 5000)
 
-        Action_Tracker.add_action({'action': 'jump', 'time': random.randint(0, 5000)})
+        Action_Tracker.add_action({'action': 'jump', 'time': randnumber})
+
+        Action_Tracker2.add_action({'action': 'jump', 'time': randnumber})
+
+    for i in range(100000):
+        randnumber = random.randint(0, 5000)
+
+        Action_Tracker.add_action({'action': 'sprint', 'time': randnumber})
+
+        Action_Tracker2.add_action({'action': 'sprint', 'time': randnumber})
+
+    for i in range(100000):
+        randnumber = random.randint(0, 5000)
+
+        Action_Tracker.add_action({'action': 'hop', 'time': randnumber})
+
+        Action_Tracker2.add_action({'action': 'hop', 'time': randnumber})
+
+    for i in range(100000):
+        randnumber = random.randint(0, 5000)
+
+        Action_Tracker.add_action({'action': 'bow', 'time': randnumber})
+
+        Action_Tracker2.add_action({'action': 'bow', 'time': randnumber})
+
+    a1 =perf_counter_ns()
+    print(Action_Tracker.get_stats())
+    a1s = perf_counter_ns()
+
+    a2 = perf_counter_ns()
+    print(Action_Tracker2.get_stats())
+    a2s = perf_counter_ns()
+
+    print(f"Post Process of averaging took: {a1s-a1} nanoseconds")
+    print(f"Running sum averaging took {a2s-a2} nanoseconds")
+
 
 
 
